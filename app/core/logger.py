e9443b41a44584dba5004ab7c8898920b018dfc0
abosaad010280-sinai <abosaad010280@gmail.com>
2026-06-15 03:42:40 +0300

    Add logging configuration

---
+++ app/core/logger.py
@@ -0,0 +1,26 @@
+"""Logging configuration"""
+
+import sys
+from loguru import logger as loguru_logger
+from app.core.config import settings
+
+# Remove default handler
+loguru_logger.remove()
+
+# Add console handler
+loguru_logger.add(
+    sys.stdout,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    level="DEBUG" if settings.DEBUG else "INFO",
+)
+
+# Add file handler
+loguru_logger.add(
+    "logs/app.log",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {name}:{function}:{line} - {message}",
+    level="DEBUG",
+    rotation="500 MB",
+    retention="7 days",
+)
+
+logger = loguru_logger
